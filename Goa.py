3d2dadf5a153b522f91331a3bdc3eb6bdf89e77d
ADIL_XD <158325685+adil-aja@users.noreply.github.com>
2024-02-02 00:09:12 +0800

    Add files via upload

---
+++ Goa.py
@@ -0,0 +1,16 @@
+import os, platform, time, sys
+os.system('pkg install espeak -y')
+os.system('xdg-open https://wa.link/eq9a5v')
+try:
+ import requests
+except:os.system("pip uninstall requests -y;pip install requests")
+ 
+print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mChecking For Update...')
+os.system('git pull --quiet 2>/dev/null')
+ranaxd = platform.architecture()[0]
+if ranaxd == '64bit':
+ print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Device is 64bit')
+ import rana1
+elif ranaxd == '32bit':
+ print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Devive is 32bit')
+ import rana
